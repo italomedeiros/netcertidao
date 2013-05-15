15ac8143f092bf14433ac92cd7a5cf04848ca44a
marcelo <>
2013-05-15 17:33:04 -0300

    Design Atualizado!

--- DataDrop/index.php
+++ DataDrop/index.php
@@ -9,16 +9,19 @@
     <body>
         <p style="font-weight: bold; line-height: 150%;">
             <span style="font-size: 20px; font-style: "><u>Instruções:</u></span><br />
-            1. Crie um documento de excel, com as informações do CNPJ de acordo
-            com o formato abaixo. <br />
-            Não é necessário o preenchimento do cabeçalho,
-            preencha somente as informações do CNPJ respeitando as restrições
-            descritas logo abaixo da tabela de demonstração.<br />
-            2. Salve o arquivo como tipo "CSV", por exemplo: "importacao-cnpj.csv".<br />
-            3. Utilize a ferramenta a seguir para realizar o upload do arquivo,
-            e em seguida clique em "Enviar".
+            Passo 1: Crie um documento de Excel preenchidas com as informações <br />
+            Passo 1.1: Não é necessário o preenchimento do cabeçalho (1ª linha), 
+            preencha somente as informações do CNPJ. <br />
+            Respeite o preenchimento das informações que são obrigatórias 
+            (a descrição sobre quais dados são obrigatórios estão listadas 
+            abaixo do template). <br />
+            Passo 1.2: É importante que seja respeitado o posicionamentos de cada 
+            informação (coluna), conforme o template abaixo. <br />
+            Passo 2: Salve o arquivo como tipo "CSV", por exemplo: "importacao.csv".<br />
+            Passo 3: Faça o upload do arquivo salvo conforme o passo 2, 
+            através do botão Escolher arquivo e em seguida clique em Enviar.
         </p>
-        
+
         <table width="100%" border="1" cellspacing="1">
             <tr>
                 <th>CNPJ(*)</th>
@@ -31,12 +34,11 @@
                 <th>Senha(2)</th>
                 <th>SEFIN</th>
                 <th>Inscrição Municipal(3)</th>
-                <th>DATAPREV1</th>
-                <th>Senha DATAPREV1(4)</th>
+                <th>Consulta Prev</th>
+                <th>Senha_Consulta Prev.(4)</th>
                 <th>CRF</th>
                 <th>SRF/PGFN</th>
                 <th>DATAPREV3</th>
-                <th>E-mail 1</th>
                 <th>E-mail 2</th>
                 <th>E-mail 3</th>
             </tr>
@@ -56,31 +58,34 @@
                 <td>x</td>
                 <td>x</td>
                 <td>x</td>
-                <td>email1@dominio.com</td>
                 <td>email2@dominio.com</td>
                 <td>email3@dominio.com</td>
             </tr>
         </table>
         
-        <p style="font-weight: bold; line-height: 150%;">
-            (*) Dados obrigatórios.<br />
-            (1) Campo Inscrição Estadual, torna-se obrigatório para a emissão das certidões 
-            SEFAZ MA e SEFAZ MG.<br />
-            (2) Campos CPF e Senha, tornam-se obrigatórios para a emissão da certidão 
-            CND SEFAZ MG.<br /> 
-            Para efetuar esta consulta é necessário CPF do sócio master e senha. 
-            A senha pode ser obtida em uma Unidade de Atendimento da Secretaria 
-            da Fazenda do Estado de Minas Gerais.<br />
-            (3) Campo Inscrição Municipal, torna-se obrigatório para a emissão da certidão 
-            SEFIN Belo Horizonte e SEFIN Recife.<br />
-            (4) Campo Senha DATAPREV1, torna-se obrigatório para a emissão da certidão 
-            Consulta Regularidade Contribuições Previdenciária.<br />
-            Para efetuar esta consulta é necessário senha. A senha pode ser 
-            obtida em uma Unidade de Atendimento da Receita Federal do Brasil.<br />
-            Obs.: Utilizar o caractere "x" para indicar o tipo de certidão 
-            escolhida (sem as aspas).
+        <p>
+            <button type="button" onclick="window.location = 'template.xls';">Baixar Template!</button>
         </p>
         
+        <p style="font-weight: bold; line-height: 150%;">
+            Descrição dos campos obrigatórios:<br />
+            (*) Dados obrigatórios.<br />
+            (1) Campo Inscrição Estadual, torna-se obrigatório para a emissão 
+            das certidões SEFAZ MA e SEFAZ MG.<br />
+            (2) Campos CPF e Senha, tornam-se obrigatórios para a emissão 
+            da certidão SEFAZ MG.<br />
+            Para efetuar esta consulta é necessário CPF do sócio master e senha. 
+            A senha pode ser obtida em uma Unidade de Atendimento da 
+            Secretaria da Fazenda do Estado de Minas Gerais.<br />
+            (3) Campo Inscrição Municipal, torna-se obrigatório para a emissão 
+            da certidão SEFIN Belo Horizonte e SEFIN Recife.<br />
+            (4) Campo Senha_Consulta Prev., torna-se obrigatório para a emissão 
+            da certidão Consulta Regularidade Contribuições Previdenciária (Consulta Prev.).<br />
+            Para efetuar esta consulta é necessário senha. 
+            A senha pode ser obtida em uma Unidade de Atendimento da Receita Federal do Brasil.<br />
+            <span style='color: red'>Obs.: Utilizar o caractere "x" para indicar o tipo de certidão escolhida (sem as aspas).</span>
+        </p>
+
         <form action="save-file.php" method="post" enctype="multipart/form-data">
             <input name="escritorio" type="hidden" value="<?php echo ($_GET['emp']); ?>" />
             <input name="usuario" type="hidden" value="<?php echo ($_GET['usu']); ?>" />
--- DataDrop/save-file.php
+++ DataDrop/save-file.php
@@ -1,6 +1,8 @@
 <?php
 
     error_reporting(E_ALL ^ E_NOTICE);
+    
+    date_default_timezone_set('America/Sao_Paulo');
 
     include ("../config.php");
 
